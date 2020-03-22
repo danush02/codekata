fe8490a921dc14c333746e3c842a37067abbaa58
danush02 <30389038+danush02@users.noreply.github.com>
2020-03-22 13:07:40 +0530

    Update and rename README.md to convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ int main() {
+ unsigned long long int A,B,C;
+  cin>>A;
+  B=A* pow(10,3);
+  C=A* pow(10,5);
+  cout<<"\n"<<B;
+  cout<<"\n"<<C;
+  return 0;
+}
